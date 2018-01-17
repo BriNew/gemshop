ae0dea21472bc9fd9d097f4e67d3a728cf48acf3
Brian Newell <brian.newell@gmail.com>
2018-01-17 11:18:36 -0800

    implemented home page @media

--- src/components/widgets.js
+++ src/components/widgets.js
@@ -5,6 +5,7 @@ export class Widgets extends React.Component {
 	render() {
 		return(
 			<div>
+
 			<div>
 			<ul className="widget_titles">
 	
@@ -14,23 +15,32 @@ export class Widgets extends React.Component {
 			<div className="widget_container_container">
 				
 		    	<div className="widget_container" className="widget_container">
-				      
-			      <div id="TA_selfserveprop774" className="ta_widget_container_desktop">
-              <ul id="0hMMZifw6g" class="TA_links V8th9Pr6YN">
-                <li id="tr9bRR" class="VSIgkvQb">
-                  <a target="_blank" href="https://www.tripadvisor.com/">
-                  <img src="https://www.tripadvisor.com/img/cdsi/img2/branding/150_logo-11900-2.png"
-                  alt="TripAdvisor"/></a>
-                </li>
-              </ul>
-            </div>
-            <script src="https://www.jscache.com/wejs?wtype=selfserveprop&amp;uniq=774&amp;
-            locationId=2161569&amp;lang=en_US&amp;rating=true&amp;nreviews=3&amp;
-            writereviewlink=true&amp;popIdx=true&amp;iswide=false&amp;border=&amp;display_version=2"></script>
-            	<div className="map_widget_container">
-			      <iframe className="map_widget" width="500px" height="375px" frameborder="0" 
-			      src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJE-VJ-QsO1FQRIQFQSgU1gvc&key=AIzaSyDrWGFKHiOsKlelMMMxkl0_J_KBOCThMmU" 
-			      allowfullscreen></iframe>
+				  <div className="home_greeting_container">
+				  	<p className="home_greeting">
+				  	<p className="home_greeting_line_1">Welcome to Chapman's Gem and Mineral Shop.</p>
+				  	<p>We are open 7 days a week, 9 - 5.
+				  	Come visit the #1 rated attraction in the area, and spend time in our ever expanding museum. Check out our
+				  	reviews and location below. Hope to see you soon!</p> 
+				  	</p>
+				  </div>
+				  <div className="map_and_ta">
+				      <div id="TA_selfserveprop774" className="ta_widget_container_desktop">
+			              <ul id="0hMMZifw6g" class="TA_links V8th9Pr6YN">
+			                <li id="tr9bRR" class="VSIgkvQb">
+			                  <a target="_blank" href="https://www.tripadvisor.com/">
+			                  <img src="https://www.tripadvisor.com/img/cdsi/img2/branding/150_logo-11900-2.png"
+			                  alt="TripAdvisor"/></a>
+			                </li>
+			              </ul>
+			          </div>
+		            <script src="https://www.jscache.com/wejs?wtype=selfserveprop&amp;uniq=774&amp;
+		            locationId=2161569&amp;lang=en_US&amp;rating=true&amp;nreviews=3&amp;
+		            writereviewlink=true&amp;popIdx=true&amp;iswide=false&amp;border=&amp;display_version=2"></script>
+	            	<div className="map_widget_container">
+				      <iframe className="map_widget" width="500px" height="374px" frameborder="0" 
+				      src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJE-VJ-QsO1FQRIQFQSgU1gvc&key=AIzaSyDrWGFKHiOsKlelMMMxkl0_J_KBOCThMmU" 
+				      allowfullscreen></iframe>
+			      	</div>
 		      	</div>
 			    </div>
 			</div>
